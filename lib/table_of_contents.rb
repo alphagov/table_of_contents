ee6d26c6ead059e60b212f10f906766ec89b25df
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-12-07 13:34:42 +0000

    Import from tech-docs-template

---
+++ lib/table_of_contents.rb
@@ -0,0 +1,8 @@
+require_relative 'table_of_contents/heading'
+require_relative 'table_of_contents/headings_builder'
+require_relative 'table_of_contents/heading_tree'
+require_relative 'table_of_contents/heading_tree_builder'
+require_relative 'table_of_contents/heading_tree_renderer'
+
+module TableOfContents
+end
