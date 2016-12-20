fbbe7730cbbe8e27254e1070045267b3ecab0c9d
Josh McMillan <josh@joshmcmillan.co.uk>
2016-12-20 00:45:49 +0000

    Add configurable "max level" of headings to include
    At the moment, we include all the headings that we can find in the
    document when generating a table of contents. This is fine, but there
    are some scenarios where we want to make this configurable – only render
    headings up to h3s, for example, to reduce the size of the table of
    contents.
    
    This allows the user to pass an options hash to
    `Helpers#table_of_contents`, which can include a parameter called
    `max_level`. Any headings that are above this level will be ignored.

--- spec/heading_tree_renderer_spec.rb
+++ spec/heading_tree_renderer_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe TableOfContents::HeadingTreeRenderer, '#html' do
-  it 'returns a nested unordered list of links to headings' do
-    tree = TableOfContents::HeadingTree.new(
+  let(:tree) {
+    TableOfContents::HeadingTree.new(
       heading: nil,
       children: [
         TableOfContents::HeadingTree.new(
@@ -18,25 +18,50 @@ describe TableOfContents::HeadingTreeRenderer, '#html' do
         )
       ]
     )
+  }
 
-    html = described_class.new(tree).html
-
-    expected_html = %{
-<ul>
-  <li>
-    <a href="#apples">Apples</a>
+  let(:expected_html_with_all_headings) {
+    <<~EOF
     <ul>
       <li>
-        <a href="#apple-recipes">Apple recipes</a>
+        <a href="#apples">Apples</a>
+        <ul>
+          <li>
+            <a href="#apple-recipes">Apple recipes</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a href="#oranges">Oranges</a>
       </li>
     </ul>
-  </li>
-  <li>
-    <a href="#oranges">Oranges</a>
-  </li>
-</ul>
-}
+    EOF
+  }
 
-    expect(html.strip).to eq(expected_html.strip)
+  let(:expected_html_with_max_heading_of_one) {
+    <<~EOF
+    <ul>
+      <li>
+        <a href="#apples">Apples</a>
+      </li>
+      <li>
+        <a href="#oranges">Oranges</a>
+      </li>
+    </ul>
+    EOF
+  }
+
+  context 'without a max_level' do
+    it 'returns a nested list of links to all headings in the tree' do
+      html = described_class.new(tree).html
+      expect(html).to eq(expected_html_with_all_headings)
+    end
+  end
+
+  context 'with a max_level of 1' do
+    it 'returns a nested list of links to top-level headings only' do
+      html = described_class.new(tree, max_level: 1).html
+      expect(html).to eq(expected_html_with_max_heading_of_one)
+    end
   end
 end
--- table_of_contents.gemspec
+++ table_of_contents.gemspec
@@ -10,4 +10,5 @@ Gem::Specification.new do |s|
   s.add_dependency 'nokogiri'
   s.add_development_dependency 'rspec'
   s.add_development_dependency 'govuk-lint'
+  s.add_development_dependency 'pry'
 end
